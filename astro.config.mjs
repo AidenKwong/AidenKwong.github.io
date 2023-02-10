c869a990d4782bca1b291056b6da153fe3acd881
aidenkwong <aidenkwong5@gmail.com>
2023-02-09 19:47:38 -0500

    fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aidenkwong.github.io",
+  site: "https://aidenkwong.github.io/",
   integrations: [
     tailwind(),
     sitemap({
