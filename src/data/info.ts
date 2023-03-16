39a2d8ca51fbc5304d57587c52d5837ed1f19718
Aiden Kwong <aidenkwong5@gmail.com>
2023-03-16 05:42:41 -0400

    update info

--- src/data/info.ts
+++ src/data/info.ts
@@ -9,12 +9,12 @@ export const info = {
   I'm passionate about building software that leverages the latest technologies to solve real-world problems.
   My expertise lies in building web applications using React and Node.js and my language of choice is TypeScript.
   I'm a lifelong learner and I'm always looking for opportunities to improve my skills and learn new technologies.
-  `,
+  `
   ],
   technical: {
     languages: ["TypeScript", "JavaScript", "Python", "Java"],
     technologies: ["React", "Vue", "Node.js", "Express", "Docker", "AWS"],
-    databases: ["MySQL", "PostgreSQL", "MongoDB", "Redis", "ElasticSearch"],
+    databases: ["MySQL", "PostgreSQL", "MongoDB", "Redis", "ElasticSearch"]
   },
 
   experience: [
@@ -29,8 +29,8 @@ export const info = {
         "- Worked with 3 and 5 software engineers in two teams for SDLC of e-commerce and logistics products, and communicated with the product manager and operation team for their needs via Atlassian products (Jira and Confluence), serving thousands of users and companies",
         "- Created a mobile app with Flutter, dashboard web pages with Vue, and servers with Node.js for both products, with GitLab as our DevOps tool and AWS as our deployment platform to maintain robust and secured hosting and deployment for service reliability",
         "- Implemented REST and API Gateway services integrated with Swagger and authentication using an SOA server-side framework called Nestjs, notifications with Firebase Cloud Messaging, and a full-text search function with ElasticSearch which decreased navigation time by about 30 seconds",
-        "- In addition to building features and maintenance, also built CI/CD pipelines, Docker, and deploy through ECS for teams to facilitate development workflow and consistency by reducing repetitive tasks which consume a couple of minutes each time",
-      ],
+        "- In addition to building features and maintenance, also built CI/CD pipelines, Docker, and deploy through ECS for teams to facilitate development workflow and consistency by reducing repetitive tasks which consume a couple of minutes each time"
+      ]
     },
     {
       name: "Data Visualization Developer (internship)",
@@ -40,9 +40,9 @@ export const info = {
       startDate: "Jan 2022",
       endDate: "Mar 2022",
       description: [
-        "- Created scrollytelling websites showcasing interactive data visualization charts and UI for the general public, with the leverage of d3.js, and other related frameworks and libraries which attracted thousands to follow us on social media",
-      ],
-    },
+        "- Created scrollytelling websites showcasing interactive data visualization charts and UI for the general public, with the leverage of d3.js, and other related frameworks and libraries which attracted thousands to follow us on social media"
+      ]
+    }
   ],
 
   education: [
@@ -52,16 +52,16 @@ export const info = {
       startDate: "2017",
       endDate: "2022",
       description: [
-        "Bachelor of Engineering (Hons) in Mechanical and Automation Engineering",
-      ],
-    },
+        "Bachelor of Engineering (Hons) in Mechanical and Automation Engineering"
+      ]
+    }
   ],
 
   socialMedia: {
     twitter: "https://twitter.com/aidenkwong5",
     github: "https://github.com/AidenKwong",
     email: "aidenkwong5@gmail.com",
-    linkedin: "https://www.linkedin.com/in/aidenkwong/",
+    linkedin: "https://www.linkedin.com/in/aidenkwong/"
   },
 
   projects: [
@@ -71,16 +71,16 @@ export const info = {
       isFeatured: true,
       thumbnail: "/assets/images/dezktop.png",
       githubUrl: "https://github.com/aidenkwong/Dezktop",
-      liveUrl: "https://dezktop.vercel.app/",
+      liveUrl: "https://dezktop.vercel.app/"
     },
     {
       title: "Youtube Popular Videos Archiver",
-      description: ` A server archives global YouTube popular videos daily`,
+      description: ` A server archives global YouTube popular videos daily\nWarning: It takes some time to load the data as the backend needs cold start`,
       isFeatured: true,
       thumbnail: "/assets/images/ympv.png",
       githubUrl:
         "https://github.com/aidenkwong/Youtube-Popular-Video-API-Backend",
-      liveUrl: "https://ympv.live/",
+      liveUrl: "https://ympv.live/"
     },
     {
       title: "Awesome Go Package Rank",
@@ -88,7 +88,7 @@ export const info = {
       isFeatured: true,
       thumbnail: "/assets/images/go-package-rank.png",
       githubUrl: "https://github.com/aidenkwong/go-package-rank",
-      liveUrl: "https://aidenkwong.me/go-package-rank/",
-    },
-  ],
+      liveUrl: "https://aidenkwong.me/go-package-rank/"
+    }
+  ]
 };
